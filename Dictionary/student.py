a6bce96414402c09fd805fc87034113080279685
Ghulam Muhammad Khizar <ghulammuhammadkhizar@gmail.com>
2026-07-05 15:29:52 +0500

    Update student.py

--- Dictionary/student.py
+++ Dictionary/student.py
@@ -1,8 +1,56 @@
+# student = {
+#     'Name': 'Ali',
+#     'Age': 20,
+#     'Department': 'Computer Science'
+# }
+
+# for i in student:
+#     print(student[i])
+
+
+# book = {
+#     "Name": "python basics",
+#     "Author": "Jhon",
+#     "price": "500"}
+
+# print(book["Name"])
+
+# student = {
+#     "name": "ali",
+#     "age": 20,
+#     "department": "CS"
+# }
+
+# student["age"] = 21
+
+# print(student)
+
+
+# student = {
+#     "name": "ali",
+#     "age": 20,
+
+# }
+
+# student["department"] = "CS"
+
+# print(student)
+
+
+# car = {
+#     "Brand": "toyota",
+#     "Model": "corolla",
+#     "year": 2020
+# }
+# del (car["year"])
+
+# print(car)
+
+
 student = {
-    'Name': 'Ali',
-    'Age': 20,
-    'Department': 'Computer Science'
+    "name": "ali",
+    "age": 20,
 }
 
-for i in student:
-    print(student[i])
+print(student.get("name"))
+print(student.get("age"))
