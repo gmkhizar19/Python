aa6d35c399414d9cdf133bfe29853f4d9b839d6c
Ghulam Muhammad Khizar <ghulammuhammadkhizar@gmail.com>
2026-07-13 16:15:38 +0500

    Update encoding1.py

--- encoding1.py
+++ encoding1.py
@@ -4,5 +4,12 @@ import urllib.error
 
 
 fhndle = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+
+counts = dict()
+
 for line in fhndle:
-    print(line.decode().strip())
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)
