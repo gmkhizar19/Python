198c01569c24b2d477fb4e16a6f83966331a7ca2
Ghulam Muhammad Khizar <ghulammuhammadkhizar@gmail.com>
2026-07-17 21:29:09 +0500

    commit

---
+++ class2.py
@@ -0,0 +1,19 @@
+class PartyAnimal:
+    def __init__(self):
+        self.x = 0
+        print("I am constructed")
+
+    def party(self):
+        self.x = self.x + 1
+        print("So far", self.x)
+
+    def __del__(self):
+        print("i am deconstructed", self.x)
+
+
+an = PartyAnimal()
+
+an.party()
+an.party()
+an = 42
+print("an contains", an)
